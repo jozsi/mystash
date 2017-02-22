b758e38c7dec7b1c6d9ca7c1b6b0707964461774
Józsi <git@jozsi.ro>
2017-02-22 16:14:28 +0200

    Add watch command

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 const Koa = require('koa');
-const config = require('./config');
 
 const app = new Koa();
 
 app.use(async (ctx) => {
   ctx.body = 'Hello World';
 });
-
-if (!module.parent) {
-  app.listen(config.HTTP_PORT, () => {
-    console.log(`Server running on port ${config.HTTP_PORT}`);  // eslint-disable-line no-console
-  });
-}
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-require('./app');
+const app = require('./app');
+const config = require('./config');
+
+if (!module.parent) {
+  app.listen(config.HTTP_PORT, () => {
+    console.log(`Server running on port ${config.HTTP_PORT}`);  // eslint-disable-line no-console
+  });
+}
