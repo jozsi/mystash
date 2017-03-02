c79907be21f6d2a9a285626740219d4b5a839911
Józsi <git@jozsi.ro>
2017-03-03 00:33:49 +0200

    Improved error handling

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const app = require('./app');
-const CONFIG = require('./config');
+const { DB_URI, HTTP_PORT } = require('./config');
 const db = require('./db');
 const router = require('./routes');
 
-app.listen(CONFIG.HTTP_PORT, async function onListening() {
-  await db.connect(CONFIG.DB_URI);
-  console.log(`Running [DB:${db.connection.name}] [HTTP:${this.address().port}]`);  // eslint-disable-line no-console
+const onInit = () => {
   app.use(router.routes());
-});
+  console.log(`Running [DB:${db.connection.name}] [HTTP:${HTTP_PORT}]`);  // eslint-disable-line no-console
+};
+
+const onError = ({ message }) => {
+  console.error(`Error: ${message}`);  // eslint-disable-line no-console
+  process.exit(1);
+};
+
+Promise
+  .all([
+    new Promise((resolve, reject) => app.listen(HTTP_PORT, resolve).on('error', reject)),
+    db.connect(DB_URI),
+  ])
+  .then(onInit)
+  .catch(onError);
