ecc305c016672580388e3a031fd95f1a581063e1
Józsi <git@jozsi.ro>
2017-03-12 20:04:36 +0200

    Add ListPlaceholder

--- src/components/WalletsList.jsx
+++ src/components/WalletsList.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import List from 'grommet/components/List';
 import ListItem from 'grommet/components/ListItem';
+import ListPlaceholder from 'grommet-addons/components/ListPlaceholder';
 
-const WalletsList = ({ list }) => (
+const WalletsList = ({ list, isLoading }) => (
   <List>
     {list.map(item => (
       <ListItem justify="between" key={item.id}>
@@ -10,15 +11,23 @@ const WalletsList = ({ list }) => (
         <span className="secondary">{item.formattedBalance}</span>
       </ListItem>
     ))}
+    <ListPlaceholder
+      emptyMessage={`Oh, I see we don't have any data yet.
+        Don't worry, we'll be on the path to saving money in no time! Let's create a wallet`}
+      unfilteredTotal={list.length}
+      filteredTotal={isLoading ? undefined : list.length}
+    />
   </List>
 );
 
 WalletsList.propTypes = {
   list: React.PropTypes.arrayOf(React.PropTypes.object),
+  isLoading: React.PropTypes.bool,
 };
 
 WalletsList.defaultProps = {
   list: [],
+  isLoading: false,
 };
 
 export default WalletsList;
