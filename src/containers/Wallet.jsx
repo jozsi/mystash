4162db7383940e4f9c320e6219d8d44da653c7af
Józsi <git@jozsi.ro>
2017-04-05 01:13:00 +0300

    Transaction details w/ currency

--- src/containers/Wallet.jsx
+++ src/containers/Wallet.jsx
@@ -1,21 +1,26 @@
 import moment from 'moment';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { read } from '../actions/transaction';
+import { read as readTransaction } from '../actions/transaction';
+import { read as readWallet } from '../actions/wallet';
 import Table from '../components/Table';
+import { byId } from '../reducers/wallet';
 import currencyValue from '../../isomorphic/currencyValue';
-
-const TRANSACTION_TABLE = new Map([
-  ['Date', row => moment(row.date).format('L')],
-  ['Details', row => row.details],
-  ['Amount', row => currencyValue(row.amount, 'USD')],  // TODO: remove hardcoded currency
-]);
 
 class Wallet extends Component {
   componentDidMount() {
-    const { match: { params } } = this.props;
-    this.props.read(params.id);
+    const { match: { params }, wallet } = this.props;
+    if (!wallet.id) {
+      this.props.readWallet();
+    }
+    this.props.readTransaction(params.id);
   }
+
+  TRANSACTION_TABLE = new Map([
+    ['Date', row => moment(row.date).format('L')],
+    ['Details', row => row.details],
+    ['Amount', row => currencyValue(row.amount, this.props.wallet.currency)],
+  ]);
 
   render() {
     const { transactionList } = this.props;
@@ -23,23 +28,41 @@ class Wallet extends Component {
     return (
       <Table
         rows={transactionList}
-        columns={TRANSACTION_TABLE}
+        columns={this.TRANSACTION_TABLE}
       />
     );
   }
 }
 
 Wallet.propTypes = {
-  transactionList: React.PropTypes.arrayOf(React.PropTypes.shape({})),
-  read: React.PropTypes.func.isRequired,
-  match: React.PropTypes.shape({}).isRequired,
+  transactionList: React.PropTypes.arrayOf(React.PropTypes.shape({
+    amount: React.PropTypes.number,
+    date: React.PropTypes.string,
+    details: React.PropTypes.string,
+  })),
+  wallet: React.PropTypes.shape({
+    currency: React.PropTypes.string,
+  }),
+  readTransaction: React.PropTypes.func.isRequired,
+  readWallet: React.PropTypes.func.isRequired,
+  match: React.PropTypes.shape({
+    params: React.PropTypes.object,
+  }).isRequired,
 };
 
 Wallet.defaultProps = {
   transactionList: [],
+  wallet: {},
 };
 
-const mapStateToProps = ({ transaction }) => ({ transactionList: transaction.list });
-const mapDispatchToProps = { read };
+const mapStateToProps = ({ transaction, wallet }, { match: { params } }) => ({
+  transactionList: transaction.list,
+  wallet: byId(wallet, params.id),
+});
+
+const mapDispatchToProps = {
+  readTransaction,
+  readWallet,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,31 +1,41 @@
 import { WALLET_READ_REQUEST, WALLET_READ_SUCCESS, WALLET_READ_FAILURE, WALLET_CREATE_SUCCESS } from '../actions';
 
-const initialState = { list: [] };
+const initialState = { list: [], ids: {} };
 
 const reducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case WALLET_READ_REQUEST:
       return { ...state, isLoading: true };
-    case WALLET_READ_SUCCESS:
-      return {
+    case WALLET_READ_SUCCESS: {
+      const nextState = {
         ...state,
         list: payload,
         isLoading: false,
       };
+      nextState.list.forEach((item, i) => {
+        nextState.ids[item.id] = i;
+      });
+      return nextState;
+    }
     case WALLET_READ_FAILURE:
       return {
         ...state,
         error: (payload.response || {}).error || payload.message,
         isLoading: false,
       };
-    case WALLET_CREATE_SUCCESS:
-      return {
+    case WALLET_CREATE_SUCCESS: {
+      const nextState = {
         ...state,
         list: [...state.list, payload],
       };
+      nextState.ids[payload.id] = nextState.list.length - 1;
+      return nextState;
+    }
     default:
       return state;
   }
 };
 
 export default reducer;
+
+export const byId = (state, id) => state.list[state.ids[id]];
