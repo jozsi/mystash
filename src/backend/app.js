566bc6dc8a67a2b2b2748f3330dc182bf87f49d8
Józsi <git@jozsi.ro>
2017-08-08 19:45:55 +0300

    Serve built files

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,14 +1,22 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
+const history = require('koa-connect-history-api-fallback');
+const serve = require('koa-static');
 const router = require('./routes');
 
 const app = new Koa();
 
 app.context.router = router;
-app.use(bodyParser());
+
+app.use(history({ htmlAcceptHeaders: ['text/html'] }));
+app.use(serve('build', { index: false }));
+
 app.use(async (ctx, next) => {
   try {
     await next();
+    if (ctx.response.status === 404 && !ctx.response.body) {
+      ctx.throw(404);
+    }
   } catch (err) {
     ctx.status = err.statusCode || err.status || 500;
     ctx.body = {
@@ -16,6 +24,7 @@ app.use(async (ctx, next) => {
     };
   }
 });
+app.use(bodyParser());
 app.use(router.routes());
 
 module.exports = app;
--- src/backend/index.js
+++ src/backend/index.js
@@ -1,12 +1,10 @@
 require('react-scripts/config/env');
 const app = require('./app');
 const db = require('./db');
-const router = require('./routes');
 
 const { API_PORT, DB_URI } = process.env;
 
 const onInit = () => {
-  app.use(router.routes());
   console.log(`Running [DB:${db.connection.name}] [HTTP:${API_PORT}]`);  // eslint-disable-line no-console
 };
 
