5a7951b0ed3740ae66201ad30f8db96956571ec2
Józsi <git@jozsi.ro>
2017-08-22 23:44:07 +0300

    Use .env.sample

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,16 +1,30 @@
 require('react-scripts/config/env');
+const dotenv = require('dotenv');
+const fs = require('fs');
 const app = require('./app');
 const db = require('./db');
+
+const onError = ({ message }) => {
+  console.error(`Error: ${message}`); // eslint-disable-line no-console
+  process.exit(1);
+};
+
+if (!process.env.DOTENV_LOADED) {
+  const dotenvSample = '.env.sample';
+  if (fs.existsSync(dotenvSample)) {
+    console.error(`WARNING! Missing .env file, falling back to ${dotenvSample}`); // eslint-disable-line no-console
+    dotenv.config({
+      path: dotenvSample,
+    });
+  } else {
+    onError(new Error(`Missing .env file, and no fallback found (${dotenvSample})`));
+  }
+}
 
 const { API_PORT, DB_URI } = process.env;
 
 const onInit = () => {
   console.log(`Running [DB:${db.connection.name}] [HTTP:${API_PORT}]`); // eslint-disable-line no-console
-};
-
-const onError = ({ message }) => {
-  console.error(`Error: ${message}`); // eslint-disable-line no-console
-  process.exit(1);
 };
 
 Promise
