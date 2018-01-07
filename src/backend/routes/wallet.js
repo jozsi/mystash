7821d438c238857e436d265f9215fa245eaae722
Radu <radulucaciu@gmail.com>
2018-01-07 21:33:50 +0200

    WiP: Added message to forecasting comparison

--- src/backend/routes/wallet.js
+++ src/backend/routes/wallet.js
@@ -45,11 +45,50 @@ router.get('/:id', async (ctx) => {
   const daysInPreviousMonth = Calendar.daysInMonth(Calendar.getFirstDayOfPreviousMonth());
   const previousMonthRunningExpenses = DataPrep.createRunningTotal(previousMonthExpensesIndexed, daysInPreviousMonth);
   
+  // Todo: Move this to an external file
+  // Get end of month values
+  let currentMonthTotal = 0;
+  let message = {};
+  // Todo: Handle the case where no prediction available (no actual datapoints either)
+  // Todo: Handle the case when we don't have previous data
+  if (daysLeft > 0) {
+    // We're looking at last day of prediction
+    currentMonthTotal = normalizedForecast.runningTotal.slice(-1)[0];
+    // Todo: Use an (html?) templating library for this
+    message = {
+      save: "Great job! You're on track to save {0} vs last month.",
+      equal: "It looks like you're gonna be spending about the same as last month.",
+      exceed: "Careful! If you don't change your expenses, you're going to spend {0} more than last month."
+    };
+  } else {
+    currentMonthTotal = runningExpenses.runningTotal.slice(-1)[0];
+    message = {
+      save: "Great job! You're saving {0} vs last month.",
+      equal: "It looks like you've spending about the same as last month.",
+      exceed: "Uh-oh, looks like you've spent {0} more than last month."
+    };
+  }
+  let previousMonthTotal = previousMonthRunningExpenses.runningTotal.slice(-1)[0];
+
+  let userMessage;
+  console.log(previousMonthTotal, currentMonthTotal);
+  // Todo: Make this an aproximation instead of strict comparison (i.e. $0.01 is not really saving)
+  if (currentMonthTotal < previousMonthTotal) {
+    userMessage = message.save;
+  } else if (currentMonthTotal > previousMonthTotal) {
+    userMessage = message.exceed;
+  } else {
+    userMessage = message.equal;
+  }
+
+  const re = new RegExp("\\{0\\}", "g");
+  userMessage = userMessage.replace(re, (currentMonthTotal - previousMonthTotal))
 
   ctx.body = wallet;
   ctx.actual = runningExpenses;
   ctx.forecast = normalizedForecast;
   ctx.previous = previousMonthRunningExpenses;
+  ctx.forecastMessage = userMessage;
 });
 
 router.put('/:id', async (ctx) => {
