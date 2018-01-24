95bb8db32f3ea9073ac2cde8c54cfc37d04cc80b
Józsi <git@jozsi.ro>
2018-01-24 17:37:45 +0200

    Fix build

--- cosmos.config.js
+++ cosmos.config.js
@@ -3,4 +3,7 @@ module.exports = {
   webpackConfigPath: 'react-scripts/config/webpack.config.dev',
   publicPath: 'public',
   proxiesPath: 'src/frontend/cosmos.proxies',
+  globalImports: [
+    'grommet/grommet-hpe.min.css',
+  ],
 };
