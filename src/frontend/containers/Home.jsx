22d006701126c42cff9915ddd0a994a5e03a4617
Józsi <git@jozsi.ro>
2018-05-16 23:05:15 +0300

    Polish exchange rates

--- src/frontend/containers/Home.jsx
+++ src/frontend/containers/Home.jsx
@@ -7,6 +7,7 @@ import Button from 'grommet/components/Button';
 import Distribution from 'grommet/components/Distribution';
 import Heading from 'grommet/components/Heading';
 import Quote from 'grommet/components/Quote';
+import Value from 'grommet/components/Value';
 import Add from 'grommet/components/icons/base/Add';
 import { logout } from '../actions/user';
 import { read, create } from '../actions/wallet';
@@ -14,20 +15,34 @@ import { read as readRate } from '../actions/rate';
 import Table from '../components/Table';
 import WalletAdd from '../components/WalletAdd';
 
-const getWalletDistribution = walletList => walletList.map(x => ({
-  value: x.balance,
-  labelValue: x.formattedBalance,
-  label: x.name,
-}));
+function precisionRound(number, precision = 2) {
+  var factor = Math.pow(10, precision);
+  return Math.round(number * factor) / factor;
+}
 
 class Home extends Component {
   state = {
     addVisible: false,
+    total: 0,
   };
 
   componentDidMount() {
-    this.props.readWallet().then(({ payload = [] }) => payload.forEach(({ currency }) => this.props.readRate(`USD_${currency}`)));
+    this.props.readWallet()
+      .then(({ payload = [] }) => Promise.all(payload.map(({ currency }) => this.props.readRate(`USD_${currency}`))))
+      .then(() => {
+        let total = 0;
+        this.props.wallet.list.forEach(({ balance, currency }) => total += this.getRate(balance, currency));
+        this.setState({ total });
+      });
   }
+
+  getRate = (balance, currency) => this.props.rate[`USD_${currency}`] ? balance / this.props.rate[`USD_${currency}`] : balance;
+
+  getWalletDistribution = () => this.props.wallet.list.map(x => ({
+    value: this.getRate(x.balance, x.currency),
+    labelValue: x.formattedBalance,
+    label: x.name,
+  }));
 
   toggleAdd = () => this.setState({ addVisible: !this.state.addVisible });
 
@@ -38,11 +53,14 @@ class Home extends Component {
 
   WALLET_TABLE = new Map([
     ['Name', row => row.name],
-    ['Balance', row => `${row.formattedBalance} ($${this.props.rate['USD_' + row.currency] * row.balance})`],
+    ['Balance', row => `${row.formattedBalance}${row.currency !== 'USD' && this.props.rate['USD_' + row.currency] ? ` ($${precisionRound(this.getRate(row.balance, row.currency))})` : ''}`],
   ]);
 
   render() {
-    const { wallet, history } = this.props;
+    const {
+      wallet,
+      history,
+    } = this.props;
     const { addVisible } = this.state;
 
     return (
@@ -75,8 +93,13 @@ class Home extends Component {
           emptyMessage="Oh, I see we don't have any data yet. Don't worry, we'll be on the path to saving money in no time! Let's create a wallet..."
           isLoading={wallet.isLoading}
         />
+        <Value
+          value="$"
+          units={precisionRound(this.state.total)}
+          label="Total"
+        />
         <Distribution
-          series={getWalletDistribution(wallet.list)}
+          series={this.getWalletDistribution()}
           size="small"
         />
       </Box>
